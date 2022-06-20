cfd9fd4813973f57390449eb113d043c86d255bc
Daniel Müller <daniel.mueller.aurich@gmail.com>
2022-06-20 05:47:23 +0000

    exemption for hashtags in preprocessing-step added

--- tweet_processor.py
+++ tweet_processor.py
@@ -29,7 +29,7 @@ class TweetProcessor:
 
         for tweet_dict in tweet_dicts:
             is_retweet = tweet_dict['quoted_tweet_id'] is not None
-               
+
             # skip tweet without text and retweets (no benefits for the embedding phase)
             if len(tweet_dict['text']) == 0 or (filter_out_retweets and is_retweet):
                 continue
@@ -57,12 +57,13 @@ class TweetProcessor:
         return unaccented_string
 
     @staticmethod
-    def _to_lowercase(text: str, exclude_acronyms: bool = False) -> str:
+    def _to_lowercase(text: str, exclude_acronyms: bool = False, exclude_hashtags: bool = False) -> str:
         """ Lowercases the entire input text except acronyms. """
         lowercased_tokens = []
         for token in text.split():
-            if exclude_acronyms:
-                lowercased_token = token.lower() if not TweetProcessor._is_acronym(token) else token
+            if (exclude_acronyms and TweetProcessor._is_acronym(token)) \
+                or (exclude_hashtags and TweetProcessor._is_hashtag(token)):
+                lowercased_token = token
             else:
                 lowercased_token = token.lower()
             lowercased_tokens.append(lowercased_token)
@@ -78,26 +79,69 @@ class TweetProcessor:
         return re.sub(TweetProcessor.WHITE_LIST, '', text).strip()
 
     @staticmethod
-    def _add_common_ngrams(tweets: List[Tweet]) -> None:
+    def _remove_ngrams_involving_hashtags(tweet_tokens_with_ngrams: List[str]) -> List[str]:
+        filtered_tokens = []
+        for token in tweet_tokens_with_ngrams:
+            if '_' in token and '#' in token:
+                filtered_tokens.extend(token.split('_'))
+            else:
+                filtered_tokens.append(token)
+    
+        return filtered_tokens
+
+    @staticmethod
+    def _add_common_ngrams(tweets: List[Tweet], ignore_ngrams_involving_hashtags: bool = False) -> None:
         ngrams = gensim.models.Phrases([tweet.tokens for tweet in tweets])
         for tweet in tweets:
             tweet_tokens_with_ngrams = ngrams[tweet.tokens]
+
+            if ignore_ngrams_involving_hashtags:
+                tweet_tokens_with_ngrams = TweetProcessor._remove_ngrams_involving_hashtags(tweet_tokens_with_ngrams)
+            
             tweet.text = ' '.join(tweet_tokens_with_ngrams)
 
     @staticmethod
     def _remove_stopwords(tokens: List[SpacyToken]) -> List[SpacyToken]:
-        return [token for token in tokens if not token.is_stop and token.text not in TweetProcessor.WORDS_TO_REMOVE]
+        return [
+            token for token in tokens
+            if not token.is_stop and token.text not in TweetProcessor.WORDS_TO_REMOVE
+        ]
 
     @staticmethod
-    def _lemmatize(tokens: List[SpacyToken], ignore_acronyms: bool = True) -> List[SpacyToken]:
+    def _is_hashtag_spacy(tokens: List[SpacyToken], token_index: int) -> bool:
+        return token_index > 0 and tokens[token_index - 1].text == '#'
+
+    @staticmethod
+    def _is_hashtag(token: str) -> bool:
+        return TweetProcessor.HASHTAG_REGEX.match(token) is not None
+
+    @staticmethod
+    def _lemmatize(
+        tokens: List[SpacyToken], ignore_acronyms: bool, ignore_hashtags: bool) -> List[SpacyToken]:
         lemmatized_tokens = []
-        for token in tokens:
-            if ignore_acronyms and TweetProcessor._is_acronym(token.text):
+        for i, token in enumerate(tokens):
+            if (ignore_acronyms and TweetProcessor._is_acronym(token.text)) \
+                or (ignore_hashtags and TweetProcessor._is_hashtag_spacy(tokens, i)):
                 lemmatized_tokens.append(token.text)
             else:
                 lemmatized_tokens.append(token.lemma_)
 
         return lemmatized_tokens
+
+    @staticmethod
+    def _convert_tokens_to_tweet_text(tokens: List[str]) -> str:
+        is_hashtag = False
+        tweet_text = ''
+        for token in tokens:
+            if token == '#':
+                is_hashtag = True
+            elif is_hashtag:
+                tweet_text += ' #' + token
+                is_hashtag = False
+            else:
+                tweet_text += ' ' + token
+
+        return tweet_text.strip()
 
     @staticmethod
     def _remove_stopwords_and_lemmatize(tweets: List[Tweet], spacy_batch_size: int = 100) -> None:
@@ -108,10 +152,11 @@ class TweetProcessor:
         for i, doc in enumerate(nlp.pipe(texts, batch_size=spacy_batch_size, n_process=4)):
             tokens = list(doc)
             tokens = TweetProcessor._remove_stopwords(tokens)
-            tokens = TweetProcessor._lemmatize(tokens)
+            tokens = TweetProcessor._lemmatize(tokens, ignore_acronyms=True, ignore_hashtags=True)
 
-            cleaned_tweet = ' '.join(tokens)
-            tweets[i].text = cleaned_tweet
+            cleaned_tweet_text = TweetProcessor._convert_tokens_to_tweet_text(tokens)
+
+            tweets[i].text = cleaned_tweet_text
 
             if i % (spacy_batch_size * 20) == 0:
                 print(f'Removing stopwords and lemmatizing with spacy: [{i} / {len(tweets)}]')
@@ -122,7 +167,7 @@ class TweetProcessor:
         return [tweet for tweet in tweets if len(tweet.hashtags) > 0]
 
     @staticmethod
-    def _remove_single_word_tweets(tweets: List[Tweet]) -> List[Tweet]: 
+    def _remove_single_word_tweets(tweets: List[Tweet]) -> List[Tweet]:
         # NOTE: our dataset contains 445 single word tweets
         return [tweet for tweet in tweets if len(tweet.tokens) > 1]
 
@@ -137,19 +182,19 @@ class TweetProcessor:
             ]
 
     def preprocess_tweets_for_word_embedding(self, file_path: Path) -> List[Tweet]:
-        tweet_corpus = self._read_tweets_from_file(file_path, filter_out_retweets=False)
+        tweets = self._read_tweets_from_file(file_path, filter_out_retweets=False)
 
-        for tweet in tweet_corpus:
+        for tweet in tweets:
             tweet.text = self._remove_urls(tweet.text)
             tweet.text = self._remove_user_names(tweet.text)
             tweet.text = self._handle_accented_characters(tweet.text)
-            tweet.text = self._to_lowercase(tweet.text, exclude_acronyms=True)
+            tweet.text = self._to_lowercase(tweet.text, exclude_acronyms=True, exclude_hashtags=True)
             tweet.text = self._only_keep_letters_spaces_underscores_hashtags(tweet.text)
 
-        self._add_common_ngrams(tweet_corpus)
-        self._remove_stopwords_and_lemmatize(tweet_corpus)
+        self._add_common_ngrams(tweets, ignore_ngrams_involving_hashtags=True)
+        self._remove_stopwords_and_lemmatize(tweets)
 
-        return tweet_corpus
+        return tweets
 
     def preprocess_tweets_for_sentence_embedding(self, file_path: Path) -> List[Tweet]:
         """ Keeps punctuation, emoij's, stopwords and doesn't lemmatize as keeping the tweets mostly in their original form
@@ -173,5 +218,3 @@ class TweetProcessor:
         self._add_common_ngrams(tweets)
 
         return tweets
-
-        
