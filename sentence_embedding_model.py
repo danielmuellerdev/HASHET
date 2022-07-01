ef204c01526a9a5081fd9fb67ff8826c3696ca28
Daniel Müller <daniel.mueller.aurich@gmail.com>
2022-07-01 17:46:37 +0000

    Memory bug in SentenceEmbeddingModel fixed

--- sentence_embedding_model.py
+++ sentence_embedding_model.py
@@ -4,6 +4,8 @@ from transformers import BertTokenizer, BertModel
 import torch
 
 class SentenceEmbeddingModel:
+    OUTPUT_DIM = 768
+
     def __init__(self):
         self._tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
         self._model = BertModel.from_pretrained('bert-base-uncased')
@@ -14,7 +16,7 @@ class SentenceEmbeddingModel:
         last_hidden_states = outputs[0].squeeze()  # The last hidden-state is the first element of the output tuple
         last_hidden_state_at_cls_token = last_hidden_states[0]
         
-        return last_hidden_state_at_cls_token  
+        return last_hidden_state_at_cls_token.detach()
 
     def generate_embeddings(self, sentences: List[str]) -> torch.tensor:
         sents_embs = [self._generate_embedding(sent) for sent in sentences]
