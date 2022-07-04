674ebfb2d6bdcd1388424a1cd9f3f567caab8ee2
Daniel Müller <daniel.mueller.aurich@gmail.com>
2022-07-04 17:23:18 +0000

    Added HashtagVisualizer

---
+++ hashtag_visualizer.py
@@ -0,0 +1,143 @@
+from typing import List, Tuple, Dict
+from pathlib import Path
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import torch
+from torch import Tensor
+from matplotlib.axes import Axes
+
+from word_embedding_model import WordEmbeddingModel
+from hashtag_to_sent_mapper import Hashtag2SentMapper
+from tweet import Tweet
+
+
+class HashtagVisualizer:
+    COLORS = [
+        'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 
+        'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan'
+    ]
+
+    def __init__(
+        self, sent_emb_tweets: List[Tweet], word_emb_model: WordEmbeddingModel, 
+        hashtag_to_sent_mapper: Hashtag2SentMapper
+    ):
+        self._word_emb_model = word_emb_model
+        self._hashtag_to_sent_mapper = hashtag_to_sent_mapper
+        
+        self._pca = PCA(n_components=2)
+        
+        self.unique_hashtags = set(hashtag for tweet in sent_emb_tweets for hashtag in tweet.hashtags)
+        self.unique_hashtags = word_emb_model.remove_hashtags_not_part_of_the_vocab(self.unique_hashtags)
+
+    @staticmethod
+    def _get_hashtag_embeddings(
+        hashtags: List[str],
+        word_emb_model: WordEmbeddingModel,
+        hashtag_to_sent_mapper: Hashtag2SentMapper = None,
+        after_transformation: bool = False
+    ) -> Tensor:
+        hashtag_embs = [word_emb_model.get_embedding(hashtag) for hashtag in hashtags]
+
+        if after_transformation:
+            with torch.no_grad():
+                hashtag_embs = [hashtag_to_sent_mapper(hashtag_emb) for hashtag_emb in hashtag_embs]
+        
+        return torch.stack(hashtag_embs)
+
+    @staticmethod
+    def _reduce_to_two_dimensions(tensor: Tensor, pca: PCA) -> Tensor:
+        return pca.fit_transform(tensor)
+
+    def plot_all_hashtags(self, after_transformation: bool = False) -> None:
+        hashtag_embs = self._get_hashtag_embeddings(
+            self.unique_hashtags, self._word_emb_model, self._hashtag_to_sent_mapper, after_transformation
+        )
+        two_dim_hashtag_embs = self._reduce_to_two_dimensions(hashtag_embs, self._pca)
+
+        x, y = two_dim_hashtag_embs[:, 0], two_dim_hashtag_embs[:, 1]
+
+        plt.scatter(x, y)
+
+        fig = plt.gcf()
+        fig.set_size_inches(20, 10)
+        
+        plt.title(
+            'All Hashtags in the Hashtag-embedding space '
+            + ('[after transformation]' if after_transformation else '[Word2Vec]')
+        )
+        plt.savefig(
+            'save_files/'
+            + 'all_hashtags_' + ('transformed' if after_transformation else 'word2vec') + '.png'
+        )
+        plt.show()
+
+    @staticmethod
+    def _read_topics_file(
+        topics_file_path: Path, word_emb_model: WordEmbeddingModel
+    ) -> Tuple[List[str], List[str]]:
+        hashtags, topics = [], []
+        with open(topics_file_path, encoding='utf-8') as file:
+            for line in file.readlines()[1:]: # skip header line
+                hashtag, topic = line.strip().split(',')
+
+                if hashtag not in word_emb_model.vocab:
+                    continue
+
+                hashtags.append(hashtag)
+                topics.append(topic)
+        
+        return hashtags, topics
+
+    @staticmethod
+    def _label_selected_hashtags(
+        hashtags: List[str], topics: List[str], ax: Axes, 
+        num_hashtags_to_label_per_topic: int, x: Tensor, y: Tensor
+    ) -> None:
+        topic_to_num_hashtags_labeled = defaultdict(int)
+        for i, (hashtag, topic) in enumerate(zip(hashtags, topics)):
+            if topic_to_num_hashtags_labeled[topic] < num_hashtags_to_label_per_topic:
+                ax.annotate(hashtag, (x[i], y[i]))
+
+                topic_to_num_hashtags_labeled[topic] += 1
+
+    def plot_hashtags_with_topics(
+        self, topics_file_path: Path, after_transformation: bool = False,
+        num_hashtags_to_label_per_topic: int = 3
+     ):
+        hashtags, topics = self._read_topics_file(topics_file_path, self._word_emb_model)
+
+        hashtag_embs = self._get_hashtag_embeddings(
+            hashtags, self._word_emb_model, self._hashtag_to_sent_mapper, after_transformation
+        )
+        two_dim_hashtag_embs = self._reduce_to_two_dimensions(hashtag_embs, self._pca)
+
+        x, y = two_dim_hashtag_embs[:, 0], two_dim_hashtag_embs[:, 1]
+        
+        fig, ax = plt.subplots()
+        fig.set_size_inches(20, 10)
+        
+        topic_to_color_num = {topic: i for i, topic in enumerate(set(topics))}
+        topic_to_stats = defaultdict(list)
+        for i, topic in enumerate(topics):
+            topic_to_stats[topic].append((x[i], y[i]))
+
+        for topic, stats in topic_to_stats.items():
+            xs, ys = [x for x, _ in stats], [y for _, y in stats]
+            color = HashtagVisualizer.COLORS[topic_to_color_num[topic]]
+            ax.scatter(xs, ys, c=color, label=topic)
+
+        ax.legend()
+
+        self._label_selected_hashtags(hashtags, topics, ax, num_hashtags_to_label_per_topic, x, y)
+        
+        plt.title(
+            'Selected Hashtags with their topics in the Hashtag-embedding space '
+            + ('[after transformation]' if after_transformation else '[Word2Vec]')
+        )
+        plt.savefig(
+            'save_files/'
+            + 'selected_hashtags_with_topics_' + ('transformed' if after_transformation else 'word2vec') + '.png'
+        )
+        plt.show()
