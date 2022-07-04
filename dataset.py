46243ba231a0a0c768a9123fd1778d6e04a3f07e
Daniel Müller <daniel.mueller.aurich@gmail.com>
2022-07-04 17:23:51 +0000

    DataModule now takes num_workers as parameter

--- dataset.py
+++ dataset.py
@@ -221,10 +221,10 @@ class DataModule(pl.LightningDataModule):
         return self.train_dataset[0]['y'].shape[0]
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
